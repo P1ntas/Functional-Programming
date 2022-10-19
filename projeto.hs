e73706e71d148f40e03fc76dd64690b2f13460cb
AfonsoAbreu1922 <up202008552@edu.fe.up.pt>
2022-10-20 00:29:26 +0100

    normalização, soma, multiplicação e parse ligeiramente corrigido. Ainda tem erros

--- projeto.hs
+++ projeto.hs
@@ -1,15 +1,14 @@
-Type Poly = [Int, [Char, Int]]
+import Data.List.Split
+import Data.Text.Encoding.Error (ignore)
+type Mono = (Int, [(Char, Int)])
+type Poly = [Mono]
 
-parseString :: String -> [[[String]]]
-parseString [] = [[[]]]
-parseString s = splitCoefficient (split '+' (removeSpace (addPlus s)))
 
-split :: Char -> String -> [String]
-split _ "" = []
-split a b = x : (split a s)
-    where x = takeWhile (/=a) b
-          s = drop (length x + 1) b
+--------------------------parsing-------------------------------
 
+parseString :: String -> [[String]] {-, [(Char, Int)])]-}
+parseString [] = []{-("", [])-}
+parseString s = splitCoefficient (splitOneOf "+" (removeSpace (addPlus s)))
 
 addPlus :: String -> String
 addPlus [] = ""
@@ -19,27 +18,61 @@ addPlus (x:xs) | x == '-' = "+-" ++ addPlus xs
 removeSpace :: String -> String
 removeSpace s = [x | x <- s, x/=' ']
 
-splitCoefficient :: [String] -> [[[String]]]
+splitCoefficient :: [String] -> [[String]]
 splitCoefficient [] = []
-splitCoefficient (x:xs) =  [removePower (split '*' x)] ++ splitCoefficient xs
+splitCoefficient (x:xs) =  removePower (splitOn "*" x) : splitCoefficient xs
 
-removePower :: [String] -> [[String]]
+removePower :: [String] -> [String]
 removePower  [] = []
-removePower (x:xs) = [split '^' x] ++ removePower xs
+removePower (x:xs) = splitOn "^" x ++ removePower xs
 
-addPoly :: [Int] -> [Int] -> [Int]
-addPoly (p:ps) (q:qs) = (p+q):addPoly ps qs
-addPoly []      qs    = qs
-addPoly ps      []    = ps
+{-
+multCoefficient :: String -> String
+multCoefficient "" = []
+multCoefficient (x:xs) 
+  | '^' : x = ignore
+  |otherwise head = x * multCoefficient xs
+-}
 
-multPoly :: [Int] -> [Int] -> [Int]
+--------------------------------------------------------------------
+
+----------------------------Normalise poly--------------------------
+
+add1Poly :: Poly -> Poly
+add1Poly [] = []
+add1Poly [x] = [x]
+add1Poly (Mono x1 [(y1, z1)]) (Mono x2 [(y2, z2)]) | (y1 == y2) && (z1 == z2) = (Mono (x1 + x2) [(y1,z1)])
+
+--------------------------------------------------------------------
+
+-----------------------------Sum Polys---------------------------------
+
+mergePoly :: Poly -> Poly -> Poly
+mergePoly [] [] = []
+mergePoly [] x = []
+mergePoly x [] = []
+mergePoly (x:xs) (y:ys) = x : y : mergePoly xs ys
+
+addPolys :: Poly -> Poly -> Poly
+addPolys [] [] = []
+addPolys [] a = a
+addPolys a [] = a
+addPolys a b = add1Poly (mergePoly a b)
+
+----------------------------Mult Polys-----------------------------
+
+
+multPoly :: Poly -> Poly -> Poly
 multPoly []      qs      = []
 multPoly ps      []      = []
-multPoly (a0:ps) (b0:qs) = (a0*b0) : rest
-  where
-    rest = addPoly (addPoly rest1 rest2) rest3
-    rest1 = [a0*q | q<-qs]
-    rest2 = [b0*p | p<-ps]
-    rest3 = 0 : multPoly ps qs
+multPoly (x:xs) (y:ys) = (Mono x1 [(y1, z1)]) (Mono x2 [(y2, z2)]) if y1 == y2 then (Mono (x1 * x2) [(y1, z1+z2)]) else (Mono (x1*x2) [(y1, z2), (y2, z2)])
 
-  
+
+---------------------------------Some values-----------------------------
+a = Mono 3 [('x', 3)]
+b = Mono 6 [('y', 2)]
+c = Mono 2 [('z', 5)]
+d = Mono 9 [('x', 5)]
+
+f = Poly [a]
+h = Poly [d]
